f56a713954f53c55e34353e88d04436b377afed3
Terrance Griffith <tjgriffi@ucsd.edu>
2016-09-20 19:51:09 -0700

    Added function for min ht Tree

--- Graph.cpp
+++ Graph.cpp
@@ -2,9 +2,16 @@ public class Node{
 
 	public List<Node> _children;
 	public bool _marked;
+	public int _data;
+	public Node _left;
+	public Node _right;
 
 	public Node(){
 		_marked = false;
+	}
+
+	public Node(int data){
+		_data = data;
 	}
 
 	public addChildren(Node child){
@@ -36,5 +43,29 @@ public class Node{
 		}
 
 		return false;
+	}
+
+	public void buildTreeMinHt(int[] arr){
+		MyQueue q = new MyQueue();
+		int i = 0;
+		Node curr = new Node(arr[i]);
+		while(i < arr.length){
+			if (((2^i)+1) < arr.length)
+			{
+				curr._left = new Node(arr[(2^i)+1]);
+				q.add(curr._left);
+				if (((2^1)+2) < arr.length)
+				{
+					curr._right = new Node(arr[(2^i)+2]);
+					q.add(curr._right);
+				}
+			}
+
+			curr = q.remove();
+			i++;
+			if(((2^i)+1) >= arr.length){
+				return;
+			}
+		}
 	}
 }
